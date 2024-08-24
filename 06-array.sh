64fc1da3f6586340e74d262b33574c0686bcbc46
Mohansai <bandarumohansai@gmail.com>
2024-08-24 21:27:30 +0530

    using arrays in shell scripting

---
+++ 06-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#This help you on how to use array in shell scripting
+
+Fruits=("Apples" "Oranges" "Grapes")
+
+echo "Provided array contains 3 fruits, Oth element in given list is: $Fruits[0]"
+echo "In the given list of array, second element is: $Fruits[02]"
