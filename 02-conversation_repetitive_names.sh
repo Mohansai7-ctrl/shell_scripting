1dea3047e141a35ba754bd48b22a3f6b7444ff1d
Mohansai <bandarumohansai@gmail.com>
2024-08-24 20:22:49 +0530

    Conversation without variables, Conversation using variables and conversation using arguments at run time

---
+++ 02-conversation_repetitive_names.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Ramesh and suresh having small conversation
+
+echo "Ramesh:: Hi Suresh, How are you?"
+echo "Suresh:: Hello Ramesh, I'm good, How do you do?
---
+++ 03-conversation_usingvariables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#To reduce errors and reduce the efforts, we are giving the names as variables as below:
+
+Person1=Ramesh
+Person2=Suresh
+
+echo "$Person1:: Hi ${Person2}, How are you?"
+echo "${Person2}:: Hello $Person1, I'm good, How do you do?"
