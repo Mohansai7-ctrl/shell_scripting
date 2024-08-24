94d3eaf3dc969709afb3ca432174c6c64b0e0cb6
Mohansai <bandarumohansai@gmail.com>
2024-08-24 20:32:23 +0530

    Giving username and password by read and secret modes

---
+++ 05-array_givingpasswordsecretly.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Here im giving the username and password in both read mode and secret modes.
+
+echo "Please enter your Username:"
+read Username
+echo "Hi, My Username is %Username"
+
+#giving pwd secrectly by using -s
+
+echo "Now enter your password:"
+read -s Password
+echo "your password is: $Password" 
+echo "Thank you, you logged in"
