e4d2de5cd53d3b67fa2f61c67ea46417e4d04af3
Mohansai <bandarumohansai@gmail.com>
2024-08-26 12:46:32 +0530

    Did few changes in array file and now executing in linux which is in Superputty

--- 06-array.sh
+++ 06-array.sh
@@ -6,3 +6,4 @@ Fruits=("Apples" "Oranges" "Grapes")
 
 echo "Provided array contains 3 fruits, Oth element in given list is: $Fruits[0]"
 echo "In the given list of array, second element is: $Fruits[02]"
+echo "All the fruits in given list are: $Fruits[@]"
