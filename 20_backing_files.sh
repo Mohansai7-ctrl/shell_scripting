fc85b47e12497efda9daa0bc6d613ba3cfbc7d15
Mohansai <bandarumohansai@gmail.com>
2024-09-01 08:00:32 +0530

    backing up script

--- 20_backing_files.sh
+++ 20_backing_files.sh
@@ -1,62 +1,80 @@
 #!/bin/bash
 
+# SOURCE_DIR=$1
+# DESTINATION_DIR=$2
+# DAYS=${3:-14} #Here user can provide $3 as optional, if user didnt provide this then 14 can be taken as default days
+
+# TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+# ZIP_FILE="$DESTINATION_DIR-$TIMESTAMP.zip"
+
+# CHECKS(){
+#     echo "Directory exists"
+# }
+
+# INPUTS(){
+#     if [[ $# -lt 2 ]]
+#     then
+#         echo " You have to provide minimum two arguments in this format: <sh 20_backing up the log files.sh> <Source directory path> <Destination directory path> <files which are older than no of days(optional)>"
+#         exit 1
+#     else
+#         echo "As you provided both the directories paths, we are proceeding further"
+#     fi
+# }
+
+# INPUTS
+
+# if [ -d $SOURCE_DIR ]
+# then
+#     CHECKS
+# else
+#     echo "Source dir doesn't exists"
+# fi
+
+# if [ -d $DESTINATION_DIR ]
+# then
+#     CHECKS
+# else
+#     echo "Destination dir doesn't exists"
+# fi
+
+# LOG_FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
+
+# if [ ! -z "$LOG_FILES" ]
+# then
+#     echo "Yes, this directory consists of older files which are more than $DAYS"
+#     $LOG_FILES | zip "$ZIP_FILE" -@
+#     if [ -f $ZIP_FILE ]
+#     then
+#         echo "As zip file exists, going to delete older files from source directory"
+#         while IFS= read -r file
+#         do
+#         echo "As zipping to destination folder done, now deleting these older files from source directory"
+#         rm -rf $file
+#         done <<< $LOG_FILES
+#     else
+#         echo "Some issue with zipping the file, need to check it"
+#     fi
+
+# else
+#     echo "No files found which are older than 14 days"
+# fi
+
 SOURCE_DIR=$1
-DESTINATION_DIR=$2
-DAYS=${3:-14} #Here user can provide $3 as optional, if user didnt provide this then 14 can be taken as default days
-
+DEST_DIR=$2
+DAYS=${3:-14} #if $3 is empty, default is 14 days.
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-ZIP_FILE="$DESTINATION_DIR-$TIMESTAMP.zip"
 
-CHECKS(){
-    echo "Directory exists"
+USAGE(){
+    echo -e "$R USAGE:: $N sh 19-backup.sh <source> <destination> <days(optional)>"
 }
+#check the source and destination are provided
 
-INPUTS(){
-    if [[ $# -lt 2 ]]
-    then
-        echo " You have to provide minimum two arguments in this format: <sh 20_backing up the log files.sh> <Source directory path> <Destination directory path> <files which are older than no of days(optional)>"
-        exit 1
-    else
-        echo "As you provided both the directories paths, we are proceeding further"
-    fi
-}
-
-INPUTS
-
-if [ -d $SOURCE_DIR ]
+if [ $# -lt 2 ]
 then
-    CHECKS
+    USAGE
+    exit 1
 else
-    echo "Source dir doesn't exists"
-fi
-
-if [ -d $DESTINATION_DIR ]
-then
-    CHECKS
-else
-    echo "Destination dir doesn't exists"
-fi
-
-LOG_FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
-
-if [ ! -z "$LOG_FILES" ]
-then
-    echo "Yes, this directory consists of older files which are more than $DAYS"
-    $LOG_FILES | zip "$ZIP_FILE" -@
-    if [ -f $ZIP_FILE ]
-    then
-        echo "As zip file exists, going to delete older files from source directory"
-        while IFS= read -r file
-        do
-        echo "As zipping to destination folder done, now deleting these older files from source directory"
-        rm -rf $file
-        done <<< $LOG_FILES
-    else
-        echo "Some issue with zipping the file, need to check it"
-    fi
-
-else
-    echo "No files found which are older than 14 days"
+    echo "Provided required arguments, hence proceeding further"
 fi
 
 
