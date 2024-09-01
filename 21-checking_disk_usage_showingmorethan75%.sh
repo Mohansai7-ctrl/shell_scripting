9ff738fcb971af7a939272e9534a92247f861a2d
Mohansai <bandarumohansai@gmail.com>
2024-09-01 10:19:45 +0530

    disk usage more than threshold script

--- 21-checking_disk_usage_showingmorethan75%.sh
+++ 21-checking_disk_usage_showingmorethan75%.sh
@@ -6,40 +6,28 @@
 
 
 
-# DISK_USAGE=$(df -hT | grep xfs)
-# Threshold=5
-
-# G="\e[32m"
-# N="\e[0m"
-
-# while IFS= read -r line
-# do
-# usage=$($line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
-# Partition=$($line | grep xfs | awk -F " " '{print $NF}')
-
-# if [[ $usage -ge $Threshold ]]
-# then
-#     echo -e "$G The file systems which are consuming more disk usage are: $usage and those are mounted on $Partition $N "
-# fi
-
-
-# done <<< $DISK_USAGE
-
-
-
-
-
-
 DISK_USAGE=$(df -hT | grep xfs)
-DISK_THRESHOLD=5 #real projects, it is usually 75
+Threshold=5
 
+G="\e[32m"
+N="\e[0m"
 
-while IFS= read -r line #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
+while IFS= read -r line
 do
-    USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
-    PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}')
-    if [ $USAGE -ge $DISK_THRESHOLD ]
-    then
-        echo "$PARTITION is more than $DISK_THRESHOLD, current value: $USAGE. Please check"
-    fi
+usage=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
+Partition=$(echo $line | grep xfs | awk -F " " '{print $NF}')
+
+if [ $usage -gt $Threshold ]
+then
+    echo -e "$G The file systems which are consuming more disk usage are: $usage and those are mounted on $Partition $N "
+fi
+
+
 done <<< $DISK_USAGE
+
+
+
+
+
+
+
