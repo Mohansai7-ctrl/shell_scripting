8c8c6f4b3bd965647d13d3ba0e1d459cac5bf07b
Mohansai <bandarumohansai@gmail.com>
2024-09-01 10:01:46 +0530

    disk usage more than threshold script

--- 21-checking_disk_usage_showingmorethan75%.sh
+++ 21-checking_disk_usage_showingmorethan75%.sh
@@ -14,10 +14,10 @@ N="\e[0m"
 
 while IFS= read -r line
 do
-usage=$($DISK_USAGE | awk -F " " '{print $6F}')
-Partition=$($DISK_USAGE | awk -F " " '{print $NF}')
+usage=$($line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -F1)
+Partition=$($line | grep xfs | awk -F " " '{print $NF}')
 
-if [[ $usage -gt $Threshold ]]
+if [[ $usage -ge $Threshold ]]
 then
     echo -e "$G The file systems which are consuming more disk usage are: $usage and those are mounted on $Partition $N "
 fi
